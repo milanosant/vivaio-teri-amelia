a0e10fc31e87548525f234d283049ec3bc68bb07
Santino Milano <santinomilano04@gmail.com>
2026-06-19 18:41:30 +0200

    feat: creazione pagina Chi Siamo con mappa di Partanna e fix routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'chi-siamo', component: About }
+];
