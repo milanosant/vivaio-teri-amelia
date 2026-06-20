97d2a0a360183928f7db00414273649ecfe68a74
Santino Milano <santinomilano04@gmail.com>
2026-06-20 10:51:57 +0200

    chore: installazione Ionic framework

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; // Importiamo il postino
+import { provideHttpClient } from '@angular/common/http';
+import { provideIonicAngular } from '@ionic/angular/standalone'; // Importiamo il postino
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // Lo attiviamo per tutta l'applicazione
-  ]
+    provideHttpClient(),
+    provideIonicAngular({}), // Lo attiviamo per tutta l'applicazione
+  ],
 };
